9576f4ba6b5149258af2e0873205f6dd7740aae4
jurgen.kluft <jurgen.kluft@gmail.com>
2024-09-27 13:30:35 +0800

    * cmacos
    * nest namespace, split it (NSPrivate.hpp)
    * reformat (NSObject.hpp)

--- source/main/include/cmacos/Foundation/NSObject.hpp
+++ source/main/include/cmacos/Foundation/NSObject.hpp
@@ -35,68 +35,68 @@
 
 namespace NS
 {
-template <class _Class, class _Base = class Object>
-class _NS_EXPORT Referencing : public _Base
-{
-public:
-    _Class*  retain();
-    void     release();
+    template <class _Class, class _Base = class Object>
+    class _NS_EXPORT Referencing : public _Base
+    {
+    public:
+        _Class* retain();
+        void    release();
 
-    _Class*  autorelease();
+        _Class* autorelease();
 
-    UInteger retainCount() const;
-};
+        UInteger retainCount() const;
+    };
 
-template <class _Class, class _Base = class Object>
-class Copying : public Referencing<_Class, _Base>
-{
-public:
-    _Class* copy() const;
-};
+    template <class _Class, class _Base = class Object>
+    class Copying : public Referencing<_Class, _Base>
+    {
+    public:
+        _Class* copy() const;
+    };
 
-template <class _Class, class _Base = class Object>
-class SecureCoding : public Referencing<_Class, _Base>
-{
-};
+    template <class _Class, class _Base = class Object>
+    class SecureCoding : public Referencing<_Class, _Base>
+    {
+    };
 
-class Object : public Referencing<Object, objc_object>
-{
-public:
-    UInteger      hash() const;
-    bool          isEqual(const Object* pObject) const;
+    class Object : public Referencing<Object, objc_object>
+    {
+    public:
+        UInteger hash() const;
+        bool     isEqual(const Object* pObject) const;
 
-    class String* description() const;
-    class String* debugDescription() const;
+        class String* description() const;
+        class String* debugDescription() const;
 
-protected:
-    friend class Referencing<Object, objc_object>;
+    protected:
+        friend class Referencing<Object, objc_object>;
 
-    template <class _Class>
-    static _Class* alloc(const char* pClassName);
-    template <class _Class>
-    static _Class* alloc(const void* pClass);
-    template <class _Class>
-    _Class* init();
+        template <class _Class>
+        static _Class* alloc(const char* pClassName);
+        template <class _Class>
+        static _Class* alloc(const void* pClass);
+        template <class _Class>
+        _Class* init();
 
-    template <class _Dst>
-    static _Dst                   bridgingCast(const void* pObj);
-    static class MethodSignature* methodSignatureForSelector(const void* pObj, SEL selector);
-    static bool                   respondsToSelector(const void* pObj, SEL selector);
-    template <typename _Type>
-    static constexpr bool doesRequireMsgSendStret();
-    template <typename _Ret, typename... _Args>
-    static _Ret sendMessage(const void* pObj, SEL selector, _Args... args);
-    template <typename _Ret, typename... _Args>
-    static _Ret sendMessageSafe(const void* pObj, SEL selector, _Args... args);
+        template <class _Dst>
+        static _Dst                   bridgingCast(const void* pObj);
+        static class MethodSignature* methodSignatureForSelector(const void* pObj, SEL selector);
+        static bool                   respondsToSelector(const void* pObj, SEL selector);
+        template <typename _Type>
+        static constexpr bool doesRequireMsgSendStret();
+        template <typename _Ret, typename... _Args>
+        static _Ret sendMessage(const void* pObj, SEL selector, _Args... args);
+        template <typename _Ret, typename... _Args>
+        static _Ret sendMessageSafe(const void* pObj, SEL selector, _Args... args);
 
-private:
-    Object() = delete;
-    Object(const Object&) = delete;
-    ~Object() = delete;
+    private:
+        Object()              = delete;
+        Object(const Object&) = delete;
+        ~Object()             = delete;
 
-    Object& operator=(const Object&) = delete;
-};
-}
+        Object& operator=(const Object&) = delete;
+    };
+}  // namespace NS
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -147,7 +147,7 @@ _NS_INLINE _Dst NS::Object::bridgingCast(const void* pObj)
     return (__bridge _Dst)pObj;
 #else
     return (_Dst)pObj;
-#endif // __OBJC__
+#endif  // __OBJC__
 }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -166,7 +166,7 @@ _NS_INLINE constexpr bool NS::Object::doesRequireMsgSendStret()
 
     return std::is_class(_Type) && (sizeof(_Type) > kStructLimit);
 #else
-#error "Unsupported architecture!"
+#    error "Unsupported architecture!"
 #endif
 }
 
@@ -193,9 +193,9 @@ _NS_INLINE _Ret NS::Object::sendMessage(const void* pObj, SEL selector, _Args...
         return (*pProc)(pObj, selector, args...);
     }
     else
-#endif // ( defined( __i386__ )  || defined( __x86_64__ )  )
+#endif  // ( defined( __i386__ )  || defined( __x86_64__ )  )
 #if !defined(__arm64__)
-        if constexpr (doesRequireMsgSendStret<_Ret>())
+      if constexpr (doesRequireMsgSendStret<_Ret>())
     {
         using SendMessageProcStret = void (*)(_Ret*, const void*, SEL, _Args...);
 
@@ -207,7 +207,7 @@ _NS_INLINE _Ret NS::Object::sendMessage(const void* pObj, SEL selector, _Args...
         return ret;
     }
     else
-#endif // !defined( __arm64__ )
+#endif  // !defined( __arm64__ )
     {
         using SendMessageProc = _Ret (*)(const void*, SEL, _Args...);
 
@@ -219,17 +219,11 @@ _NS_INLINE _Ret NS::Object::sendMessage(const void* pObj, SEL selector, _Args...
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-_NS_INLINE NS::MethodSignature* NS::Object::methodSignatureForSelector(const void* pObj, SEL selector)
-{
-    return sendMessage<MethodSignature*>(pObj, _NS_PRIVATE_SEL(methodSignatureForSelector_), selector);
-}
+_NS_INLINE NS::MethodSignature* NS::Object::methodSignatureForSelector(const void* pObj, SEL selector) { return sendMessage<MethodSignature*>(pObj, _NS_PRIVATE_SEL(methodSignatureForSelector_), selector); }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-_NS_INLINE bool NS::Object::respondsToSelector(const void* pObj, SEL selector)
-{
-    return sendMessage<bool>(pObj, _NS_PRIVATE_SEL(respondsToSelector_), selector);
-}
+_NS_INLINE bool NS::Object::respondsToSelector(const void* pObj, SEL selector) { return sendMessage<bool>(pObj, _NS_PRIVATE_SEL(respondsToSelector_), selector); }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -273,30 +267,18 @@ _NS_INLINE _Class* NS::Object::init()
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-_NS_INLINE NS::UInteger NS::Object::hash() const
-{
-    return sendMessage<UInteger>(this, _NS_PRIVATE_SEL(hash));
-}
+_NS_INLINE NS::UInteger NS::Object::hash() const { return sendMessage<UInteger>(this, _NS_PRIVATE_SEL(hash)); }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-_NS_INLINE bool NS::Object::isEqual(const Object* pObject) const
-{
-    return sendMessage<bool>(this, _NS_PRIVATE_SEL(isEqual_), pObject);
-}
+_NS_INLINE bool NS::Object::isEqual(const Object* pObject) const { return sendMessage<bool>(this, _NS_PRIVATE_SEL(isEqual_), pObject); }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-_NS_INLINE NS::String* NS::Object::description() const
-{
-    return sendMessage<String*>(this, _NS_PRIVATE_SEL(description));
-}
+_NS_INLINE NS::String* NS::Object::description() const { return sendMessage<String*>(this, _NS_PRIVATE_SEL(description)); }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-_NS_INLINE NS::String* NS::Object::debugDescription() const
-{
-    return sendMessageSafe<String*>(this, _NS_PRIVATE_SEL(debugDescription));
-}
+_NS_INLINE NS::String* NS::Object::debugDescription() const { return sendMessageSafe<String*>(this, _NS_PRIVATE_SEL(debugDescription)); }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
